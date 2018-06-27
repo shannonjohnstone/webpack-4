619850da05b5efade18cf691ea4fa9aade596b42
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2018-06-28 09:21:23 +1000

    setup js minify

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,72 +3,83 @@ const webpack = require('webpack');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const UglifyjsPlugin = require('uglifyjs-webpack-plugin');
 // const isProd = precess.env.NODE_ENV === 'production';
 
-module.exports = {
-  entry: {
-    main: ["./src/main.js"]
-  },
-  mode: "production",
-  output: {
-    filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, '../dist'),
-    publicPath: "/"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader"
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCSSExtractPlugin.loader
-          },
-          {
-            loader: "css-loader"
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: {
-              attrs: ["img:src"]
+module.exports = env => {
+  return {
+    entry: {
+      main: ["./src/main.js"]
+    },
+    mode: "production",
+    output: {
+      filename: "[name]-bundle.js",
+      path: path.resolve(__dirname, '../dist'),
+      publicPath: "/"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader"
             }
-          }
-        ]
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: "images/[name].[ext]"
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCSSExtractPlugin.loader
+            },
+            {
+              loader: "css-loader"
             }
-          }
-        ]
-      },
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: {
+                attrs: ["img:src"]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(jpg|png|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: "images/[name].[ext]"
+              }
+            }
+          ]
+        },
+      ]
+    },
+    plugins: [
+      new UglifyjsPlugin(),
+      new MinifyPlugin(),
+      new OptimizeCssPlugin({
+        filename: "[name]-[contenthash].css"
+      }),
+      new MiniCSSExtractPlugin(),
+      new htmlWebpackPlugin({
+        template: './src/index.ejs',
+        title: 'Link\'s Journal'
+      }),
+      new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify(env.NODE_ENV)
+        }
+      })
     ]
-  },
-  plugins: [
-    new OptimizeCssPlugin({
-      filename: "[name]-[contenthash].css"
-    }),
-    new MiniCSSExtractPlugin(),
-    new htmlWebpackPlugin({
-      template: './src/index.ejs',
-      title: 'Link\'s Journal'
-    })
-  ]
+  }
 }
